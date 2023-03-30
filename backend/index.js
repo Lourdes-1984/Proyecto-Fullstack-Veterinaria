3eade0ab9b1a17534af0dd63dee513e922510886
lourdes sosa <lourdes1984.sosa@gmail.com>
2023-03-30 19:10:11 -0300

    cambios en la estructura de backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+app.use('/', (req,res) => {
+    res.send('Hola Mundo Cruel me canse')
+})
+
+const PORT = 4000
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto: ${PORT}`);
+})
